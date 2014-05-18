1446d4ce3f1aa4968ca0e5f059a863dcfea8f0d0
Nathan Burrill <nbrl@users.noreply.github.com>
2014-05-18 14:04:28 +0100

    Added bash script to auto-populate redis store for testing.

---
+++ populate.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#
+# Populate redis store for fun
+
+host="http://localhost:8080/messages"
+
+for i in 1 2 3 4 5; do
+	curl ${host} \
+		-X POST \
+		-d "{\"body\":\"${i}\"}" \
+		-H 'Content-Type: application/json'
+done
