e37cebfbd4407fbccafe9e4396af39441914f8a0
Nathan Burrill <nbrl@users.noreply.github.com>
2014-05-18 23:04:11 +0100

    Commented routing.php.

--- routing.php
+++ routing.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * Routing file for the built-in PHP web server
+ *
+ * This is the equivalent of .htaccess for Apache, but for PHPs build-in web
+ * server. It simply sends any request for a non-existent file to index.php in
+ * the document root.
+ */
 
 if (file_exists(__DIR__ . '/' . $_SERVER['REQUEST_URI'])) {
 	return false; // serve the requested resource as-is.
